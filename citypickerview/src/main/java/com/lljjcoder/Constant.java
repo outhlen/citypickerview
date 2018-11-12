85be6a4181600b77a69839e8ca8ad04a987537fc
outhlen <2595241187@qq.com>
2018-11-12 09:30:43 +0800

    修改城市文件

--- citypickerview/src/main/java/com/lljjcoder/Constant.java
+++ citypickerview/src/main/java/com/lljjcoder/Constant.java
@@ -7,5 +7,6 @@ package com.lljjcoder;
  */
 
 public class Constant {
-    public static final String CITY_DATA = "china_city_data.json";
+    //public static final String CITY_DATA = "china_city_data.json";
+	public static final String CITY_DATA = "city.json";
 }
